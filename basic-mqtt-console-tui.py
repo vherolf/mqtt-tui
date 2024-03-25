849a22851d016f72cd9f1ec4a098a3709e1d8793
vherolf <vherolf@proclos.com>
2024-03-25 14:27:38 +0100

    made a more advanced terminal with topic field added random uuid so you can start more console parallel

--- basic-mqtt-console-tui.py
+++ basic-mqtt-console-tui.py
@@ -1,31 +1,27 @@
 """
-simple mqtt console with textual and aiomqtt
+A basic mqtt console example with textual and aiomqtt
 """
-import uuid
 from aiomqtt import Client
 from textual import work, on
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal
 from textual.widgets import Header, Footer, RichLog, Input
 from textual.binding import Binding
 
 try:
     from config import MQTT_HOST, MQTT_PORT, CLIENT_ID, MQTT_USER, MQTT_PW
-    CLIENT_ID = CLIENT_ID + str(uuid.uuid1().bytes)
 except ModuleNotFoundError as _:
     MQTT_HOST = 'fill in your mqtt host here'
     MQTT_PORT = 'add your mqtt port here'
-    CLIENT_ID = 'put your client id here' + str(uuid.uuid1().bytes)
+    CLIENT_ID = 'put your client id here'
     # also set user and password if mqtt server needs it
     MQTT_USER = None
     MQTT_PW   = None
 
 class MQTTConsole(App):
-    """a simple mqtt console"""
+    """a basic mqtt console example"""
     TITLE = "MQTT Console"
     BINDINGS = [Binding(key="q", action="quit_mqtt_console", description="Quit App"),
                 Binding(key="c", action="clear_mqtt_console", description="Clear Console"),]
-    CSS_PATH = "console-tui.tcss"
 
     client = None
     # the topic you wanna publish to
@@ -33,8 +29,7 @@ class MQTTConsole(App):
 
     def compose(self) -> ComposeResult:
         yield Header(name=self.TITLE, show_clock=False)
-        yield Input(placeholder=f"Publish a mqtt message on topic {self.topic}", id='publish')
-        yield Input(placeholder=f"topic", id='topic')
+        yield Input(placeholder=f"Publish a mqtt message on topic {self.topic}")
         yield RichLog()
         yield Footer()
 
@@ -43,11 +38,8 @@ class MQTTConsole(App):
 
     @on(Input.Submitted)
     async def input_submitted(self, message: Input.Submitted) -> None:
-        if message.input.id == 'topic':
-            self.topic = message.value
-        elif message.input.id == 'publish':
-            await self.client.publish(self.topic, f"{message.value}")
-        
+        await self.client.publish(self.topic, f"{message.value}")
+
     @work(exclusive=False)
     async def mqttWorker(self):
         async with Client(MQTT_HOST, port=MQTT_PORT, identifier=CLIENT_ID, username=MQTT_USER, password=MQTT_PW) as self.client:
